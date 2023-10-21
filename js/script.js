6dc0bb7c40125083eee35a96cb621f776b39cf69
Webcog <wajahatmurtaza144@gmail.com>
2023-10-21 21:29:20 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// document.write("hello thos")
+// window.alert()
+// console.log("hello this is console log o hjh gjh fjm")
+// innerHTML
+// document.getElementById("test").innerHTML = 'hello this is js3452'
+// String
+
+// hello this is batch 9.00
+// let const var
+var x = "hello this is batch 8.00 welcome<br>"
+document.write(x)
+document.write(x)
