5053c01cb14f94911bbf4431bc26c3f69bd6bb2a
Webcog <wajahatmurtaza144@gmail.com>
2023-10-30 22:02:06 +0500

    update

---
+++ js/condition.js
@@ -0,0 +1,10 @@
+// if > doc, write .. 
+// else > clg 
+
+let x = window.prompt("PLease First Valu")
+let y = window.prompt(" Please Enter Second Value")
+let z = window.prompt("Please Enter Sign")
+
+if (z == "-"){
+    document.write(x - y)
+}
