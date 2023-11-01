2f72a8cede56d8078e2e90116335a05c58555552
Webcog <wajahatmurtaza144@gmail.com>
2023-11-01 21:37:47 +0500

    op

---
+++ js/funcation.js
@@ -0,0 +1,23 @@
+
+// event base prog lan 
+
+let x = "<h1>heelo</h1>"
+
+// global
+
+// local 
+
+
+function myFun(){
+    
+    document.getElementById('tst').style.color="red";
+}
+
+
+// myFun()
+
+
+// user-define
+// built-in
+// recursion - dsa - self-inoked
+
