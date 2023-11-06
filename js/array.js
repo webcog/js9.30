0b6b4a4eda948e483840a532855a98034ef179e4
Webcog <wajahatmurtaza144@gmail.com>
2023-11-06 21:44:53 +0500

    updte

---
+++ js/array.js
@@ -0,0 +1,14 @@
+// let x = "thid kjh lk " 
+// let y = 454545
+// arrays 
+// index number - unique 
+let z = ["ali",1345,"typr",321,"ab",23,"qw","abyug","poii"]
+// 0 ali 
+// 1 = 323
+// length
+// let a = z.length
+document.write(z[z.length-1])
+
+// let y = "hello"
+// let z = y.length
+// document.write(y[5])
---
+++ js/practice.js
@@ -0,0 +1,31 @@
+// (9/5)C+32
+
+// (celcius * 9*5) +32
+
+
+// hours * 60 
+
+
+// 15deg 
+// let c = window.prompt("Enter value in celcius")
+// let x = (c * 9/5) +32
+// document.write(x+"Farnhite")
+
+// bodmas
+
+// baracket of diivision multn add subs
+
+
+function abc(){
+    let c = window.prompt("Enter value in celcius")
+    let x = (c * 9/5) +32
+    document.write(x+"Farnhite")
+
+}
+
+function xyz(){
+    let a = window.prompt("Enter in F")
+    let y =  (a - 32) * 5/9;
+    document.write(y+" Celsius")
+}
+
